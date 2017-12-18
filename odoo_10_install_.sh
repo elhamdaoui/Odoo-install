0a67843c7813d7f9e3921458c0b122c814764d87
Elhamdaoui Abdelmajid <elhamdaouiabdelmajid@gmail.com>
2017-12-18 13:37:04 +0000

    Update and rename odoo_10_install_globalit.sh to odoo_10_install_.sh

--- odoo_10_install_.sh
+++ odoo_10_install_.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 ################################################################################
-# Script for installing Odoo V10 on Ubuntu 16.04, 15.04, 14.04 (could be used for other version too)
-# Author: Yenthe Van Ginneken
-#-------------------------------------------------------------------------------
-# This script will install Odoo on your Ubuntu 14.04 server. It can install multiple Odoo instances
-# in one Ubuntu because of the different xmlrpc_ports
-#-------------------------------------------------------------------------------
-# Make a new file:
-# sudo nano odoo-install.sh
-# Place this content in it and then make the file executable:
-# sudo chmod +x odoo-install.sh
-# Execute the script to install Odoo:
-# ./odoo-install
+# Author: Abdelmajid Elhamdaoui, Reference : Yenthe Van Ginneken
 ################################################################################
  
 ##fixed parameters
