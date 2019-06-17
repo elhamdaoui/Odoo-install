62119239c9c2f20fe8a45e8c6365d43608c2da7a
Elhamdaoui Abdelmajid <elhamdaouiabdelmajid@gmail.com>
2019-06-17 15:21:26 +0100

    Ubuntu 18.05 WKHTML

--- odoo_11_install.sh
+++ odoo_11_install.sh
@@ -36,9 +36,8 @@ REP_OCA_SERVER_TOOLS="https://github.com/OCA/server-tools.git"
 ## === Ubuntu Trusty x64 & x32 === (for other distributions please replace these two links,
 ## in order to have correct version of wkhtmltox installed, for a danger note refer to 
 ## https://www.odoo.com/documentation/8.0/setup/install.html#deb ):
-WKHTMLTOX_X64=https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
-WKHTMLTOX_X32=https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.1/wkhtmltox-0.12.1_linux-trusty-i386.deb
-
+WKHTMLTOX_X64=https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb
+WKHTMLTOX_X32=https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_i386.deb
 #--------------------------------------------------
 # Update Server
 #--------------------------------------------------
